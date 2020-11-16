4c5b0a4dbc2d9d56e9d4db1eb11a19643a9e8c03
Nicholas Nam <admin@nicholasnam.com>
2020-11-16 11:56:43 -0500

    Shorten code

--- main.js
+++ main.js
@@ -15,12 +15,11 @@ readline.question('What is the LOWEST possible number? ', lowNumInput => {
         let highNum = highNumInput;
         dealerNum = Math.floor(Math.random() * highNum + lowNum);
         readline.question(`Choose a number between ${lowNum} and ${highNum}: `, playNumInput => {
+            console.log(`Dealer had ${dealerNum}`);
             if (playNumInput > dealerNum) {
-                console.log(`Dealer had ${dealerNum}`);
                 console.log('Yay, you WON!');
                 readline.close();
             } else {
-                console.log(`Dealer had ${dealerNum}`);
                 console.log('Sorry, you LOST...');
                 readline.close();
             }
